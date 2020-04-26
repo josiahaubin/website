fcfe3466ba53099894daee1c7527d96e574de67f
Josiah Aubin <josiahaubin@gmail.com>
2020-04-26 16:12:12 -0600

    Navbar upgrades

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,73 @@
 <template>
-  <div id="app">
+  <div id="app" class="container-fluid">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <div class="row">
+        <div class="col-sm-12 col-md-4">
+          <img
+            src="./assets/Josiah Aubin 113145170(1) - Copy.jpg"
+            alt="Josiah Profile Picture"
+            id="josiahphoto"
+            class="circle"
+          />
+        </div>
+        <div class="col-sm-12 col-md-8 text-white mt-5">
+          <div id="name">
+            <h1>
+              Josiah Aubin
+              <br />Software Developer
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div id="links" class="row mx-auto mt-2">
+        <div class="col-12">
+          <router-link to="/">Home</router-link>|
+          <router-link to="/about">Resume</router-link>|
+          <router-link to="/contact">Contact</router-link>
+        </div>
+      </div>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Inconsolata", monospace;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
+#links {
+  background-color: rgb(250, 250, 250);
+  width: fit-content;
+}
+
+#josiahphoto {
+  width: auto;
+  height: 40vh;
+  border-radius: 25px;
+}
+
+#name {
+  background-color: rgb(0, 0, 0, 0.7);
+  width: fit-content;
+}
+
+#name h1 {
+  font-size: 75px;
+}
+
 #nav {
   padding: 30px;
+  background-image: url("./assets/markus-spiske-qjnAnF0jIGk-unsplash.jpg");
+  background-size: cover;
+  background-position-y: center;
+  height: 50vh;
+  margin-left: -15px;
+  margin-right: -15px;
 }
 
 #nav a {
@@ -28,5 +77,9 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+::-webkit-scrollbar {
+  display: none;
 }
 </style>
